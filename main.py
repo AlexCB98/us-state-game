d156b4bd47a98f2a1cbecaed066fdf8fbdac61c3
Alexandru <lexyus22@gmail.com>
2026-06-03 15:03:23 +0200

    Implement state guessing system

--- main.py
+++ main.py
@@ -12,23 +12,24 @@ writer = turtle.Turtle()
 writer.hideturtle()
 writer.penup()
 
-user_answer = screen.textinput(title = 'Guess the State', prompt = "What's another state's name ?")
-
 data = pandas.read_csv('50_states.csv')
 states_name = data.state.to_list()
 
-
-
 guessed_states = []
 
-if user_answer in states_name:
-    state_data = data[data.state == user_answer]
-    x = int(state_data.x.iloc[0])
-    y = int(state_data.y.iloc[0])
+while len(guessed_states) < 50:
+    user_answer = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                   prompt="Name a state's name ?").title()
 
-    writer.goto(x,y)
-    writer.write(user_answer)
-    guessed_states.append(user_answer)
+    if user_answer in states_name:
+
+        state_data = data[data.state == user_answer]
+        x = int(state_data.x.iloc[0])
+        y = int(state_data.y.iloc[0])
+        writer.goto(x, y)
+
+        writer.write(user_answer)
+        guessed_states.append(user_answer)
 
 
 
